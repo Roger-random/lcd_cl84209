8ee09931b0c15736e44cd0cd0ad9d0294dac9689
Regorlas <roger.random@outlook.com>
2022-06-05 16:45:37 -0700

    Toggle between modes with runtime button press instead of compile-time #define

--- lcd_cl84209.ino
+++ lcd_cl84209.ino
@@ -36,26 +36,31 @@ SOFTWARE.
 #define CHAR_POSITIONS 15
 #define SEGMENTS_BYTE_COUNT 16
 
-// Comment out this line out to turn off LED heartbeat
-//#define LED_HEARTBEAT
+// GPIO13 = D7 on a Wemos D1 Mini
+#define KNOB_BUTTON_PIN 13
 
-// Only one of the following should be active.
-//#define CHARACTER_SET
-//#define ALL_ON
-#define SEGMENT_KNOB
+// milliseconds to wait before acknowledging button down state
+unsigned long knob_button_wait = 500;
+// Wait until this time before acknowledging button down state
+unsigned long knob_button_next;
 
-#ifdef SEGMENT_KNOB
+typedef enum display_mode {
+  character_set = 0,
+  all_on,
+  segment_knob,
+  display_mode_max
+} display_mode;
+
+display_mode current_mode = character_set;
+
 #include <Encoder.h>
 Encoder segmentKnob(12,14); // GPIO12 = D6 on a Wemos D1 Mini, GPIO14 = D5
 long segmentKnobPosition;
 long segmentKnobOffset = 0;
 long segmentNumber=0;
-#endif
 
-#ifdef CHARACTER_SET
 // Starting point for the next subset of characters to be printed.
 uint8_t characterSetStart;
-#endif // CHARACTER_SET
 
 // The CL84209 handset sends a particular sequence of commands once a second.
 // (The very first time after power-up it skips the final 0x02, but it doesn't
@@ -121,7 +126,6 @@ void setBit(uint8_t index, uint8_t* segments)
   segments[segmentByte] |= bitFlag;
 }
 
-#ifdef CHARACTER_SET
 // Write 16 characters from the character set, eight characters per line.
 // 'setStart' selects one of 16 starting points.
 void writeCharacterSetLines(uint8_t setStart)
@@ -191,180 +195,51 @@ void writeSegmentsPattern()
   }
   Wire.endTransmission();
 }
-#endif // CHARACTER_SET
 
 void setup() {
+  pinMode(KNOB_BUTTON_PIN, INPUT);
+  knob_button_next = millis();
+
+  segmentKnobPosition = segmentKnob.read()/4;
+  segmentKnobOffset = -segmentKnobPosition;
+
+  pinMode(2, OUTPUT);
+
   Serial.begin(115200);
   Wire.begin();
-
-#ifdef ALL_ON
-  Serial.println("CL84209 LCD all on");
-#endif // ALL_ON
-
-#ifdef CHARACTER_SET
-  Serial.println("CL84209 LCD character set");
-  characterSetStart = 0;
-#endif // CHARACTER_SET
-
-#ifdef SEGMENT_KNOB
-  Serial.println("CL84209 LCD segment selection by knob");
-  segmentKnobPosition = segmentKnob.read()/4;
-  Serial.print("Segment knob start: ");
-  Serial.println(segmentKnobPosition);
-  segmentKnobOffset = -segmentKnobPosition;
-#endif
-
-#ifdef LED_HEARTBEAT
-  pinMode(2, OUTPUT);
-#endif // LED_HEARTBEAT
 }
 
-void loop() {
-  writeConfig();
-
-#ifdef ALL_ON
-  for (uint8_t line = 0; line < 2; line++)
+void checkButton()
+{
+  if (0 == digitalRead(KNOB_BUTTON_PIN) &&
+      millis() > knob_button_next)
   {
-    Wire.beginTransmission(0x3E);
-    writeLineStart(line);
-    for(uint8_t i = 0; i < CHAR_POSITIONS; i++)
+    knob_button_next = millis() + knob_button_wait;
+    switch(current_mode)
     {
-      Wire.write(0x7F);
+      case character_set:
+        Serial.println("Switching from character set mode to all on mode.");
+        current_mode = all_on;
+        break;
+      case all_on:
+        Serial.println("Switching from all on mode to segment knob mode.");
+        current_mode = segment_knob;
+        break;
+      case segment_knob:
+        Serial.println("Switching from segment knob mode to character set mode.");
+        current_mode = character_set;
+        characterSetStart = 0;
+        break;
+      default:
+        Serial.println("Oh no, fell out of switch()! Reverting to character set mode.");
+        current_mode = character_set;
+        break;
     }
-    Wire.endTransmission();
   }
-  Wire.beginTransmission(0x3E);
-  writeLineStart(2);
-  for(uint8_t i = 0; i < SEGMENTS_BYTE_COUNT; i++)
-  {
-    Wire.write(0xFF);
-  }
-  Wire.endTransmission();
-#endif // ALL_ON
+}
 
-#ifdef CHARACTER_SET
-  writeCharacterSetLines(characterSetStart);
-  writeSegmentsPattern();
-
-  if (characterSetStart++ >= 0x0F)
-  {
-    characterSetStart = 0;
-  }
-#endif
-
-#ifdef SEGMENT_KNOB
-  uint8_t segments[SEGMENTS_BYTE_COUNT]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
-  long newPos = segmentKnob.read()/4;
-  if (newPos != segmentKnobPosition)
-  {
-    Serial.print("Segment knob: ");
-    Serial.println(newPos);
-    segmentKnobPosition = newPos;
-
-    if (segmentKnobPosition + segmentKnobOffset > 127)
-    {
-      segmentKnobOffset = 127-segmentKnobPosition;
-      Serial.print("Segment max 127 reached, offset adjusted to ");
-      Serial.println(segmentKnobOffset);
-    }
-    else if (segmentKnobPosition + segmentKnobOffset < 0)
-    {
-      segmentKnobOffset = -segmentKnobPosition;
-      Serial.print("Segment min 0 reached, offset adjusted to ");
-      Serial.println(segmentKnobOffset);
-    }
-    segmentNumber = segmentKnobOffset+segmentKnobPosition;
-  }
-
-  setBit(segmentNumber, segments);
-
-  for (uint8_t line = 0; line < 2; line++)
-  {
-    bool wroteNonZero = false;
-
-    // Write out segment data, most of which will be zero so
-    // represented by '0'. One byte will be nonzero, write as hex.
-    Wire.beginTransmission(0x3E);
-    writeLineStart(line);
-    for(uint8_t i = 0; i < 8; i++)
-    {
-      uint8_t segmentDataByte = segments[i+(line*8)];
-      if (0 == segmentDataByte)
-      {
-        Wire.write(0x30); // 0
-      }
-      else
-      {
-        Wire.write(0x28); // (
-        writeDigitAsHex(segmentDataByte>>4);
-        writeDigitAsHex(segmentDataByte&0x0F);
-        Wire.write(0x29); // )
-        wroteNonZero = true;
-      }
-    }
-
-    // If this entire line was zero, we need three spaces of padding.
-    if(!wroteNonZero)
-    {
-      Wire.write(0x20);  // space
-      Wire.write(0x20);  // space
-      Wire.write(0x20);  // space
-    }
-
-    if (0==line)
-    {
-      // Line 0: write segment number as hexadecimal
-      Wire.write(0x20); // space
-      Wire.write(0x78); // x
-      writeDigitAsHex(segmentNumber>>4);
-      writeDigitAsHex(segmentNumber&0x0F);
-    }
-    else
-    {
-      // Line 1: write segment number as decimal
-      Wire.write(0x20); // space
-      if (segmentNumber >= 100)
-      {
-        Wire.write(0x30 + (segmentNumber/100));
-      }
-      else
-      {
-        Wire.write(0x20); // space
-      }
-
-      if (segmentNumber >= 10)
-      {
-        Wire.write(0x30 + ((segmentNumber%100)/10));
-      }
-      else
-      {
-        Wire.write(0x20); // space
-      }
-      if (segmentNumber >= 1)
-      {
-        Wire.write(0x30 + (segmentNumber%10));
-      }
-      else
-      {
-        Wire.write(0x30); // 0
-      }
-    }
-
-    Wire.endTransmission();
-  }
-
-  // Write out segment control data
-  Wire.beginTransmission(0x3E);
-  writeLineStart(2);
-  for(uint8_t i = 0; i < SEGMENTS_BYTE_COUNT; i++)
-  {
-    Wire.write(segments[i]);
-  }
-  Wire.endTransmission();
-#endif // SEGMENT_KNOB
-
-#ifdef LED_HEARTBEAT
+void led_heartbeat()
+{
   // LED heartbeat
   digitalWrite(2, LOW);
   delay(10);
@@ -374,5 +249,158 @@ void loop() {
   delay(10);
   digitalWrite(2, HIGH);
   delay(1000);
-#endif // LED_HEARTBEAT
+}
+
+void loop() {
+  uint8_t segments[SEGMENTS_BYTE_COUNT]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  long newPos = segmentKnob.read()/4;
+
+  checkButton();
+  writeConfig();
+
+  switch(current_mode)
+  {
+    case character_set:
+      writeCharacterSetLines(characterSetStart);
+      writeSegmentsPattern();
+
+      if (characterSetStart++ >= 0x0F)
+      {
+        characterSetStart = 0;
+      }
+      led_heartbeat();
+      break;
+    case all_on:
+      for (uint8_t line = 0; line < 2; line++)
+      {
+        Wire.beginTransmission(0x3E);
+        writeLineStart(line);
+        for(uint8_t i = 0; i < CHAR_POSITIONS; i++)
+        {
+          Wire.write(0x7F);
+        }
+        Wire.endTransmission();
+      }
+      Wire.beginTransmission(0x3E);
+      writeLineStart(2);
+      for(uint8_t i = 0; i < SEGMENTS_BYTE_COUNT; i++)
+      {
+        Wire.write(0xFF);
+      }
+      Wire.endTransmission();
+      led_heartbeat();
+      break;
+    case segment_knob:
+      if (newPos != segmentKnobPosition)
+      {
+        Serial.print("Segment knob: ");
+        Serial.println(newPos);
+        segmentKnobPosition = newPos;
+
+        if (segmentKnobPosition + segmentKnobOffset > 127)
+        {
+          segmentKnobOffset = 127-segmentKnobPosition;
+          Serial.print("Segment max 127 reached, offset adjusted to ");
+          Serial.println(segmentKnobOffset);
+        }
+        else if (segmentKnobPosition + segmentKnobOffset < 0)
+        {
+          segmentKnobOffset = -segmentKnobPosition;
+          Serial.print("Segment min 0 reached, offset adjusted to ");
+          Serial.println(segmentKnobOffset);
+        }
+        segmentNumber = segmentKnobOffset+segmentKnobPosition;
+      }
+
+      setBit(segmentNumber, segments);
+
+      for (uint8_t line = 0; line < 2; line++)
+      {
+        bool wroteNonZero = false;
+
+        // Write out segment data, most of which will be zero so
+        // represented by '0'. One byte will be nonzero, write as hex.
+        Wire.beginTransmission(0x3E);
+        writeLineStart(line);
+        for(uint8_t i = 0; i < 8; i++)
+        {
+          uint8_t segmentDataByte = segments[i+(line*8)];
+          if (0 == segmentDataByte)
+          {
+            Wire.write(0x30); // 0
+          }
+          else
+          {
+            Wire.write(0x28); // (
+            writeDigitAsHex(segmentDataByte>>4);
+            writeDigitAsHex(segmentDataByte&0x0F);
+            Wire.write(0x29); // )
+            wroteNonZero = true;
+          }
+        }
+
+        // If this entire line was zero, we need three spaces of padding.
+        if(!wroteNonZero)
+        {
+          Wire.write(0x20);  // space
+          Wire.write(0x20);  // space
+          Wire.write(0x20);  // space
+        }
+
+        if (0==line)
+        {
+          // Line 0: write segment number as hexadecimal
+          Wire.write(0x20); // space
+          Wire.write(0x78); // x
+          writeDigitAsHex(segmentNumber>>4);
+          writeDigitAsHex(segmentNumber&0x0F);
+        }
+        else
+        {
+          // Line 1: write segment number as decimal
+          Wire.write(0x20); // space
+          if (segmentNumber >= 100)
+          {
+            Wire.write(0x30 + (segmentNumber/100));
+          }
+          else
+          {
+            Wire.write(0x20); // space
+          }
+
+          if (segmentNumber >= 10)
+          {
+            Wire.write(0x30 + ((segmentNumber%100)/10));
+          }
+          else
+          {
+            Wire.write(0x20); // space
+          }
+          if (segmentNumber >= 1)
+          {
+            Wire.write(0x30 + (segmentNumber%10));
+          }
+          else
+          {
+            Wire.write(0x30); // 0
+          }
+        }
+
+        Wire.endTransmission();
+      }
+
+      // Write out segment control data
+      Wire.beginTransmission(0x3E);
+      writeLineStart(2);
+      for(uint8_t i = 0; i < SEGMENTS_BYTE_COUNT; i++)
+      {
+        Wire.write(segments[i]);
+      }
+      Wire.endTransmission();
+      break;
+    default:
+      Serial.println("Oh no, fell out of switch()! Reverting to character set mode.");
+      current_mode = character_set;
+      break;
+  }
 }
